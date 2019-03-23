1ca8f266d49a5c18fd5d2e9282a4e29edfa8017f
volidamar <volidamar@mail.ru>
2019-03-23 12:27:41 +0200

    testqqq

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -26,7 +26,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Connection $connection)
     {
-      www
+        test
 //        $q=$connection->fetchAll('select * from product');
 //print_r($q);
         
